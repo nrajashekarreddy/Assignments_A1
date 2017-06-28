5167f4c332aba0c768859afa0de349e240b0700d
nrajashekarreddy <nrajashekarreddy94@gmail.com>
2017-06-28 15:43:23 +0530

    remote machine updated

--- sshremote.sh
+++ sshremote.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+if [ $# -lt 2 ];
+then      
+    echo "usage:$0 <username> <ipaddr>"
+  
+    exit 1
+
+fi
  USERNAME=$1
  HOSTS=$2
- SCRIPT="pwd; ls"
+ SCRIPT=$3
+a="$4 "
+b=" "
+c="$6"
 for HOSTNAME in ${HOSTS} ; do
-   ssh -l "${USERNAME}" "${HOSTNAME}" "${SCRIPT}"
+   ssh -l "${USERNAME}" "${HOSTNAME}" "${SCRIPT}" "${a}" "${b}" "${c}"
 done
