4f2c7e8b94cecc534bd87ecdbfc5292dcb997f23
nrajashekarreddy <nrajashekarreddy94@gmail.com>
2017-06-28 17:20:28 +0530

    file pattern update

--- specifiedfile.sh
+++ specifiedfile.sh
@@ -1,13 +1,22 @@
-#!bin/sh
 
-echo "Enter the file name"
-
-read filename
-
-echo "Enter the word you want to find."  
-
-read word
-
- find -name "$filename" | xargs grep -i $word
+#!/bin/bash
+if [ $# -lt 2 ];
+then      
+    echo "usage:$0 <number of words> <file type>"
+  
+    exit 1
+fi
+x=1
+a=$1
+while [ $x -le $a ];
+do
+echo "enter $x pattern to search "
+    
+   read files
+   find *.$2 | xargs grep --color -i $files
+   x=$(($x + 1))
+   
+   
+done
 
 
