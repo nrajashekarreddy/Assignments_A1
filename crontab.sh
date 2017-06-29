d89f5db55a9e11f7b51afe75616f836d47950ed4
nrajashekarreddy <nrajashekarreddy94@gmail.com>
2017-06-29 13:42:59 +0530

    cronjob

--- crontab.sh
+++ crontab.sh
@@ -1,18 +1,47 @@
-#!/bin/bash
+#!/bin/sh
+
+if [ $# -eq 0 ];
+
+then
+	echo "enter mail id"
+read mailId
+
+	echo "enter threshold value"
+read threshold
+	echo "enter when you want to get alert"
+	echo "enter minutes from0-59"
+read minute
+	echo "enter hour from 0-23"
+read h
+	echo "enter date from 1-31"
+read d
+	echo "enter month fom 1-12"
+read m
+	echo "enter day of the week from 0-7"
+read W
+
+current=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
+
+	if [ "$current" -gt "$threshold" ] 
+then
+	( crontab -l ; echo "$minute $h $d $m $W sh new.sh $mailId" ) | crontab -
+	fi
+fi
+
+	if [ $# -eq 1 ];
+then
+diskusage=$(df)
 CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
-THRESHOLD=1
-if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
-   mail -s 'Disk Space Alert' nrajashekarreddy94@gmail.com << EOF
-Your root partition remaining free space is critically low
-"Clean the Disk".
-Your System has Used: $CURRENT% 
+
+    mail -s 'Disk Space Alert' $1 << EOF
+    Your root partition remaining free space is critically low 
+    "Clean the Disk".
+    Your System has Used: $CURRENT%
 EOF
-echo ">>date<<"
-echo "Your root partition remaining free space is critically low"
-echo Your System has Used: $CURRENT %
-echo "Clean the Disk"
-echo ">> end <<"
-else
-echo ">>date<<"
-echo no problem
+	echo ">>`date`<<"
+	echo "Your root partition remaining free space is critically low" 
+	echo Your System has Used: $CURRENT %
+	echo "Clean the Disk"
+	echo ">> end <<"
+	
 fi
